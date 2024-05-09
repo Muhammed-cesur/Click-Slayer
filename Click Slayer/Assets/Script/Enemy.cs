726611005c87f746a3da2e65e7eafca78bb60d4d
Muhammed Cesur <90576168+Muhammed-cesur@users.noreply.github.com>
2024-05-10 01:43:11 +0300

    Enemy Text Take Damge

--- Click Slayer/Assets/Script/Enemy.cs
+++ Click Slayer/Assets/Script/Enemy.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using TMPro;
+
 
 public class Enemy : MonoBehaviour
 {
@@ -10,8 +12,10 @@ public class Enemy : MonoBehaviour
     public float _Health;
     public float _MaxHealth;
     public float _damage;
-   public GameObject _enemy;
+    public GameObject _enemy;
+    public TextMeshProUGUI _enemyText;
 
+    private bool CanTakeDamage;
     private HealthSystem _healthSystem;
     
    
@@ -22,14 +26,16 @@ public class Enemy : MonoBehaviour
         _Health = _MaxHealth;
         _healthSystem.HealthBar›mage.fillAmount=_MaxHealth;
         _anim = GetComponent<Animator>();
+        CanTakeDamage = true;
 
     }
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) & CanTakeDamage)
         {
             TakeDamage(_damage);
         }
+        _enemyText.text = _Health.ToString();
 
     }
 
@@ -43,10 +49,16 @@ public class Enemy : MonoBehaviour
 
         if (_Health <= 0)
         {
+            CanTakeDamage = false;
             _anim.SetTrigger("Die");
             Invoke(nameof(DestroyEnemy), 1.5f);
             Debug.Log("Hasarrrr");
                 
+        }
+        else
+        {
+            CanTakeDamage = true;
+
         }
     }
 
